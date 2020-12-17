c8aa171d7474538507f07f409782e062c7f8e917
emileaerts <74113251+emileaerts@users.noreply.github.com>
2020-12-17 17:18:19 +0100

    added standardnet and pretrainnet

--- model.py
+++ model.py
@@ -1,145 +1,133 @@
+"""model.py:
+    
+    contains StandardNet, SimNet and their components together with a PretrainNet that is used to pretrain the feature extractors of SimNet
+"""
+
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from pytorch_revgrad import RevGrad
-import torch
 
 
-class fNet(nn.Module):
+class Encoder(nn.Module):
     def __init__(self):
-        super(fNet, self).__init__()
+        super(Encoder, self).__init__()
         
-        self.restored = False
-
         self.conv1 = nn.Conv2d(1, 64, 5)   # 1st conv layer INPUT [1 x 28 x 28] OUTPUT [64 x 12 x 12]
         self.conv2 = nn.Conv2d(64, 64, 5)  # 2nd conv layer INPUT [64 x 12 x 12] OUTPUT [64 x 4 x 4]
         self.conv3 = nn.Conv2d(64, 128, 5) # 3rd conv layer INPUT [64 x 4 x 4] OUTPUT [128 x 1 x 1]
+        self.bn1 = nn.BatchNorm2d(64, eps=1e-05) # batch normalisation
         self.pool = nn.MaxPool2d(2, 2, padding=1)
          
     def forward(self, x):
-        output = self.pool(F.relu(self.conv1(x)))
-        output = self.pool(F.relu(self.conv2(output)))
+        output = self.pool(F.relu(self.bn1(self.conv1(x))))
+        output = self.pool(F.relu(self.bn1(self.conv2(output))))
         output = F.relu(self.conv3(output))
         output = output.view(-1, 128 * 1 * 1)
         return output
     
-    
-class gNet(nn.Module):
+class Classifier(nn.Module):
     def __init__(self):
-        super(gNet, self).__init__()
+        super(Classifier, self).__init__()
         
-        self.restored = False
-                
-        self.conv1 = nn.Conv2d(1, 64, 5)      # 1st conv layer INPUT [1 x 28 x 28]  OUTPUT [64 x 12 x 12]
-        self.conv2 = nn.Conv2d(64, 64, 5)     # 2nd conv layer INPUT [64 x 12 x 12] OUTPUT [64 x 4 x 4]
-        self.conv3 = nn.Conv2d(64, 128, 5)    # 3rd conv layer INPUT [64 x 4 x 4]   OUTPUT [128 x 1 x 1]
-        self.pool = nn.MaxPool2d(2, 2, padding=1)
-        
+        self.fc1 = nn.Linear(128, 512)
+        self.fc2 = nn.Linear(512, 10)
          
     def forward(self, x):
-        output = self.pool(F.relu(self.conv1(x)))
-        output = self.pool(F.relu(self.conv2(output)))
-        output = F.relu(self.conv3(output))
-        output = output.view(-1, 128 * 1 *1)
+        output = F.relu(self.fc1(x))
+        output = F.log_softmax(self.fc2(output), dim=1)
         return output
-
-
-    def calcproto(self, class_list):
-
-        g_output = []
+    
+class SimilarityClassifier(nn.Module):
+    def __init__(self):
+        super(SimilarityClassifier, self).__init__()
         
-        for class_image in class_list:
-            g_output_class = self.forward(class_image)
-            g_output.append(g_output_class)
+        self.U = nn.Parameter(torch.rand(size=(512, 128), requires_grad=True))
+        self.V = nn.Parameter(torch.rand(size=(512, 128), requires_grad=True))
         
-        num_classes = len(g_output) # number of classes (10 in Digits experiment)
-        num_features = g_output_class.shape[1] # number of features (128 in Digits experiment)
+    def forward(self, encoded_features, prototypes):
                 
-        prototypes = torch.Tensor(size=torch.Size([num_classes, num_features]))
+        factor_1 = torch.matmul(self.U, encoded_features.t()) # size = (512, 32)
+        factor_2 = torch.matmul(self.V, prototypes.t()) # size = (512, 10)
         
-        class_count = 0
-        for g_class in g_output:
-            if len(g_class) > 1: # number of images in 1 class bigger than one (not to be confused with number of classes!)
-                prototype = torch.mean(g_class, dim=1) / len(g_class) # calculating the prototype for each class while testing
-            
-            else:
-                prototype = g_class # in case of training we only input one image
-            
-            prototypes[class_count] = prototype
-            
-            class_count += 1
+        h = torch.matmul(factor_1.t(), factor_2) # size = (32, 10)
+        h = F.log_softmax(h, dim=1)
+        return h
         
-        return prototypes
-    
-    
+                         
 class Discriminator(nn.Module):
     def __init__(self):
-        super().__init__()
-
-        self.restored = False
-
+        super(Discriminator, self).__init__()
         
         self.layer = nn.Sequential(
+            RevGrad(),
             nn.Linear(128, 1024),
             nn.ReLU(),
             nn.Linear(1024, 1024),
             nn.ReLU(),
-            nn.Linear(1024, 2),
-            nn.Softmax(dim=1),
-            RevGrad()
+            nn.Linear(1024, 2)
         )
         
     def forward(self, x):
         output = self.layer(x)
+        output = output/torch.max(output)
+        output = F.softmax(output, dim=1) 
+        
         return output[:, 0]
     
-    
-class hSim(nn.Module):
+class PretrainNet(nn.Module):
     def __init__(self):
-        super().__init__()
-    
+        super(PretrainNet, self).__init__()
+        
+        self.encoder = Encoder()
+        self.classifier = Classifier()
+        
+         
+    def forward(self, x):
 
-        self.restored = False
+        encode_out = self.encoder(x)
+        class_out = self.classifier(encode_out)
         
-        self.U = nn.Parameter(torch.rand(size=(128, 512), requires_grad=True))
-        self.V = nn.Parameter(torch.rand(size=(128, 512), requires_grad=True))
-        
-    def forward(self, x_batch, mu_c):
-        
-        h_batch = torch.zeros(size=[32, 10]) # batchsize = 32 maybe 'automate' this size so we don't get in trouble later      when we may change the batch size (same goes for number of classes (10))
-
-        for i in range(0, len(x_batch)):
-            x = x_batch[i]
-
-            fac1 = torch.matmul(x, self.U)
-            fac2 = torch.matmul(mu_c, self.V)
-        
-            h = torch.matmul(fac2, fac1)
-            
-            h_batch[i] = F.log_softmax(h, dim=0)
-        
-        return h_batch   
-    
+        return class_out
     
 class SimNet(nn.Module):
     def __init__(self):
-        super().__init__()
-    
-        self.restored = False
-    
-        self.fnet = fNet()
-        self.gnet = gNet()
+        super(SimNet, self).__init__()
+        
+        self.fnet = Encoder()
+        self.gnet = Encoder()
+        self.classifier = SimilarityClassifier()
         self.disc = Discriminator()
-        self.hsim = hSim()
         
+         
+    def forward(self, x, mode="train", proto_images=None, encoded_proto=None):
         
-    def forward(self, f_input, g_input):
-        # f_input is batch of images
-        # g_input should be a list (in the future maybe change to tensor?) of one (while training --> randomly chosen) or more (while testing --> every from test_batch) image(s) of every class
+        if mode == "train":
+            encoded_out = self.fnet(x)
+            encoded_proto = self.gnet(proto_images)
+            class_out = self.classifier(encoded_out, encoded_proto)
+            disc_out = self.disc(encoded_out)
+            
+        if mode == "test":
+            encoded_out = self.fnet(x)
+            class_out = self.classifier(encoded_out, encoded_proto)
+            disc_out = self.disc(encoded_out)
+            
         
-        f_output = self.fnet(f_input)
-        prototypes = self.gnet.calcproto(g_input)
+        return [class_out, disc_out, encoded_proto]
+    
+class StandardNet(nn.Module):
+    def __init__(self):
+        super(StandardNet, self).__init__()
         
-        class_output = self.hsim(f_output, prototypes)
-        disc_output = self.disc(f_output)
+        self.encoder = Encoder()
+        self.classifier = Classifier()
+        self.disc = Discriminator()
         
-        return [class_output, disc_output] 
+    def forward(self, x):
+
+        encode_out = self.encoder(x)
+        class_out = self.classifier(encode_out)
+        disc_out = self.disc(encode_out)
+        
+        return [class_out, disc_out]
